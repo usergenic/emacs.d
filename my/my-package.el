f66cf13705707632cc353ada53d66c5e7ed3ffd3
Brendan Baldwin <brendanb@google.com>
2013-02-12 10:57:21 -0800

    Added support for emacs23 which is what I'm stuck with in ubuntu at work.

--- my/my-package.el
+++ my/my-package.el
@@ -4,9 +4,11 @@
 ;;
 ;;; Code:
 
-(require 'package)
+(if (not (featurep 'package)) (load-file "~/.emacs.d/oldemacs/package.el"))
+
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+
 (package-initialize)
 
 ;;; my-package ends here
