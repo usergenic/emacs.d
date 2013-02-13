4c1d1adac23d94a2e9a0c7b03e461395c2f715c7
Brendan Baldwin <brendan@usergenic.com>
2013-02-12 23:46:56 -0800

    Update the evil settings to treat C-g as escape key since it is emacsy and doesn't do weird things like double as meta ala esc.

--- my/my-evil.el
+++ my/my-evil.el
@@ -7,3 +7,22 @@
 
 ;; remap the C-u behavior to C-b, since we stole C-ul
 (define-key evil-normal-state-map (kbd "C-b") 'universal-argument)
+
+;;; C-g as general purpose escape key sequence.
+;;;
+(defun my-esc (prompt)
+  "Functionality for escaping generally.  Includes exiting Evil insert state and C-g binding. "
+  (cond
+   ;; If we're in one of the Evil states that defines [escape] key, return [escape] so as
+   ;; Key Lookup will use it.
+   ((or (evil-insert-state-p) (evil-normal-state-p) (evil-replace-state-p) (evil-visual-state-p)) [escape])
+   ;; This is the best way I could infer for now to have C-c work during evil-read-key.
+   ;; Note: As long as I return [escape] in normal-state, I don't need this.
+   ;;((eq overriding-terminal-local-map evil-read-key-map) (keyboard-quit) (kbd ""))
+   (t (kbd "C-g"))))
+
+(define-key key-translation-map (kbd "C-g") 'my-esc)
+
+;; Works around the fact that Evil uses read-event directly when in operator state, which
+;; doesn't use the key-translation-map.
+(define-key evil-operator-state-map (kbd "C-g") 'keyboard-quit)
