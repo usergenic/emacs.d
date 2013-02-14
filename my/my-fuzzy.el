2008080af6fc1c9ed3a490ed9b446a7dc34b85d9
Brendan Baldwin <brendan@usergenic.com>
2013-02-14 11:32:53 -0800

    CMD-t always resets the fuzzy find project root based on what textmate says now.

--- my/my-fuzzy.el
+++ my/my-fuzzy.el
@@ -1,11 +1,15 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/fuzzy-find-in-project")
 (require 'fuzzy-find-in-project)
-(fuzzy-find-project-root (textmate-project-root))
 
 (defun set-fuzzy-find-project-root-to-textmate-project-root  ()
   "use textmate-project-root function to figure out where fuzzy root should be"
   (interactive)
   (fuzzy-find-project-root (textmate-project-root)))
 
-(global-set-key (kbd "M-t") 'fuzzy-find-in-project)
-(global-set-key (kbd "C-c M-t") 'set-fuzzy-find-project-root-to-textmate-project-root)
+(defun set-fuzzy-find-project-root-to-textmate-project-root-and-then-fuzzy-find-in-project ()
+  (interactive)
+  (set-fuzzy-find-project-root-to-textmate-project-root)
+  (fuzzy-find-in-project))
+
+(global-set-key (kbd "M-t") 'set-fuzzy-find-project-root-to-textmate-project-root-and-then-fuzzy-find-in-project)
+
