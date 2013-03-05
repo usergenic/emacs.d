de0837516915cca02121bfc42ff0fe794ebc6e52
Brendan Baldwin <brendan@usergenic.com>
2013-03-05 11:00:16 -0800

    Updated my-backups.el to use folder inside .emacs.d

--- my/my-backups.el
+++ my/my-backups.el
@@ -6,11 +6,16 @@
 ;;
 ;;; Code:
 
-(setq backup-directory-alist `(("." . "~/.emacs_backups")))
-(setq backup-by-copying t)
-(setq delete-old-versions t
-      kept-new-versions 6
-      kept-old-versions 2
-      version-control t)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups"))
+      backup-by-copying t    ; don't delink hardlinks
+      delete-old-versions t  ; automatically delete excess backups
+      version-control t      ; use version numbers on backups
+      kept-new-versions 6    ; how many of newest versions to keep
+      kept-old-versions 2    ; how many of oldest versions to keep
+      )
+
+(setq create-lockfiles nil)
+
+(auto-save-mode) ; turn it off!
 
 ;;; my-backups ends here
