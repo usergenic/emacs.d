270575387a960977bb0d902f57e33eb23ffcf58b
Brendan Baldwin <brendan@usergenic.com>
2013-08-15 16:23:01 -0700

    Removed tron theme.

--- elpa/tron-theme-12/tron-theme-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; tron-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("tron-theme-pkg.el" "tron-theme.el") (20758
-;;;;;;  124 77461))
-
-;;;***
-
-(provide 'tron-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; tron-theme-autoloads.el ends here
